f7c71178c799bb23c65355c3c131b772c8dc9ed9
mirza-sync <mirzasyahmi123@gmail.com>
2023-01-23 23:59:01 +0800

    css: style the modal

--- components/GoalIntput.js
+++ components/GoalIntput.js
@@ -22,7 +22,14 @@ function GoalInput(props) {
           value={goal}
           onChangeText={goalInputHandler}
         />
-        <Button title='Add Goal' onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title='Cancel' onPress={null} />
+          </View>
+          <View style={styles.button}>
+            <Button title='Add Goal' onPress={addGoalHandler} />
+          </View>
+        </View>
       </View>
     </Modal>
   )
@@ -31,9 +38,9 @@ function GoalInput(props) {
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
     marginBottom: 24,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
@@ -41,9 +48,16 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: '70%',
-    marginRight: 8,
+    width: '100%',
     padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
   },
 })
 
