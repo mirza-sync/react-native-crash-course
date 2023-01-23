859885a9742c7d8c9bf68baa6a1fa3700535878e
mirza-sync <mirzasyahmi123@gmail.com>
2023-01-23 23:43:21 +0800

    feat: Put "add goal" inside modal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 import GoalInput from './components/GoalIntput';
 import GoalItem from './components/GoalItem';
 import 'react-native-get-random-values'
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 export default function App() {
   const [goals, setGoals] = useState([])
+  const [isModalVisible, setisModalVisible] = useState(false)
 
   function addGoalHandler(goal) {
     setGoals(currentGoals => [...currentGoals, {id: uuidv4(), text: goal}])
@@ -18,7 +19,8 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title='Add New Goal' color='#5e0acc' onPress={() => setisModalVisible(true)} />
+      {isModalVisible && <GoalInput visible={isModalVisible} onAddGoal={addGoalHandler} />}
       <View style={styles.goalsContainer}>
         <FlatList
           data={goals}
--- components/GoalIntput.js
+++ components/GoalIntput.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { Button, Modal, StyleSheet, TextInput, View } from "react-native";
 
 function GoalInput(props) {
   const [goal, setGoal] = useState('')
@@ -14,15 +14,17 @@ function GoalInput(props) {
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.textInput}
-        placeholder='Your goal'
-        value={goal}
-        onChangeText={goalInputHandler}
-      />
-      <Button title='Add Goal' onPress={addGoalHandler} />
-    </View>
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your goal'
+          value={goal}
+          onChangeText={goalInputHandler}
+        />
+        <Button title='Add Goal' onPress={addGoalHandler} />
+      </View>
+    </Modal>
   )
 }
 
